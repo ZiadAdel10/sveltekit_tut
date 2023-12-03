64f87ae45918d6391f65255414123b8dcc37f9a5
Ziad Adel <ziad.adel.mahmoud@gmail.com>
2023-12-03 23:49:16 +0200

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+// import { Adapter } from "@sveltejs/kit";
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+
+// @type {import ('@sveltejs/kit').Config}
+
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  },
+};
+
+export default config;
